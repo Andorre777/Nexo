9bd72f8f3630dbe35d1dd3ce4d1687c1dd5c2ce9
Andorre777 <andorre777@gmail.com>
2025-08-09 17:51:14 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,12 @@
 // Ce fichier est un module ES. Remplace les valeurs par ta config Firebase.
 // Tu peux trouver ces valeurs dans la console Firebase -> Paramètres du projet.
 export const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "TON_PROJECT.firebaseapp.com",
-  projectId: "TON_PROJECT_ID",
-  storageBucket: "TON_BUCKET.appspot.com",
-  messagingSenderId: "TON_ID",
-  appId: "TON_APP_ID"
+    apiKey: "AIzaSyB0GCLhPQADvIJPXx9h5dxyyGFHhLB6xa0",
+    authDomain: "nexo-cf6e7.firebaseapp.com",
+    projectId: "nexo-cf6e7",
+    storageBucket: "nexo-cf6e7.firebasestorage.app",
+    messagingSenderId: "243870398222",
+    appId: "1:243870398222:web:cbcae1d3965fe51229895f",
+    measurementId: "G-B967VPQM15"
+
 };
